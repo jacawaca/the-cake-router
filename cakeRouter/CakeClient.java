5e73a5162a4e1179f7c6d6e5bd3ced2da576c5f8
jacawaca <papah4@gmail.com>
2021-05-23 21:43:44 +0200

    Java prawie cała zrobiona

---
+++ cakeRouter/CakeClient.java
@@ -0,0 +1,100 @@
+package cakeRouter;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketTimeoutException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+import udp.Config;
+
+public class CakeClient {
+	private static String adress; // wysyłka
+	private static List<String> nodeAdresses;
+	private static String message;
+	private static boolean debug = true;
+
+	public static void main(String[] args) throws IOException, Exception {
+		// Wczytanie węzłów pośrednich
+		adress = args[0];
+		Path adressesFileName = Path.of(args[1]), 
+		messageFileName = Path.of(args[2]);
+		message = Files.readString(messageFileName);
+		nodeAdresses = Files.readAllLines(adressesFileName);
+		
+		// Dodajemy adres docelowy na koniec listy węzłów
+		nodeAdresses.add(adress);
+		
+		// Wysyłka na adres
+		String stringServerAdress = nodeAdresses.get(0);
+        InetAddress serverAddress = InetAddress.getByName(stringServerAdress); //zamiast "localhost"
+        System.out.println(serverAddress);
+
+        DatagramSocket socket = new DatagramSocket(); //Otwarcie gniazda
+        
+    	// Kodujemy wiadomość. Na początku adresy potem sam msg. Obcinamy pierwszy adres
+ 		for(int i=nodeAdresses.size()-1;i>0;i--) {
+ 			message = nodeAdresses.get(i) + ";" + message;
+ 		}
+ 		
+        byte[] stringContents = message.getBytes("utf8"); //Pobranie strumienia bajtów z wiadomosci
+
+        DatagramPacket sentPacket = new DatagramPacket(stringContents, stringContents.length);
+        sentPacket.setAddress(serverAddress);
+        sentPacket.setPort(Config.PORT);
+        if(debug) System.out.println("Wysyłam"+message+" na "+serverAddress.toString());
+        socket.send(sentPacket);
+
+        DatagramPacket recievePacket = new DatagramPacket( new byte[Config.BUFFER_SIZE], Config.BUFFER_SIZE);
+        socket.setSoTimeout(1010);
+
+        try{
+            socket.receive(recievePacket);
+            System.out.println("1. węzeł otrzymał wiadomość");
+        }catch (SocketTimeoutException ste){
+            System.out.println("1. węzeł nie odpowiedział.");
+        }
+        
+        // Czekanie na odpowiedź od adresata
+        //Otwarcie gniazda z okreslonym portem
+        DatagramSocket datagramSocket = new DatagramSocket(Config.PORT);
+
+        byte[] byteResponse = "OK".getBytes("utf8");
+
+        while (true){
+
+            DatagramPacket receivedPacket
+                    = new DatagramPacket( new byte[Config.BUFFER_SIZE], Config.BUFFER_SIZE);
+
+            datagramSocket.receive(receivedPacket);
+
+            int length = receivedPacket.getLength();
+            String message =
+                    new String(receivedPacket.getData(), 0, length, "utf8");
+
+            // Port i host który wysłał nam zapytanie
+            InetAddress address = receivedPacket.getAddress();
+            int port = receivedPacket.getPort();
+
+            System.out.println(message);
+            Thread.sleep(1000); //To oczekiwanie nie jest potrzebne dla
+            // obsługi gniazda
+
+            DatagramPacket response
+                    = new DatagramPacket(
+                        byteResponse, byteResponse.length, address, port);
+
+            datagramSocket.send(response);
+            System.exit(0);
+        }
+		
+	}
+
+}
---
+++ cakeRouter/UDPNode.java
@@ -0,0 +1,186 @@
+/**
+ * 
+ */
+package cakeRouter;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.SocketTimeoutException;
+import java.net.UnknownHostException;
+
+import udp.Config;
+
+import java.util.ArrayList;  
+import java.util.Arrays;  
+import java.util.List;  
+
+/**
+ * @author Jacek Strzałkowski jacek.strzalkowski.stud@pw.edu.pl
+ *
+ */
+public class UDPNode {
+	static String prevAdress;
+	private static boolean debug=true;
+	private static String returnMessage = "Paczka dotarła cała i zdrowa.";
+	
+	 public static void main(String[] args) throws Exception{
+		 	System.out.println("Gałąź ruszyła!");
+	        //Otwarcie gniazda z okreslonym portem
+	        DatagramSocket datagramSocket = new DatagramSocket(Config.PORT);
+
+	        byte[] byteResponse = "Uzyskałem wiadomość.".getBytes("utf8");
+
+	        while (true){
+
+	            DatagramPacket receivedPacket
+	                    = new DatagramPacket( new byte[Config.BUFFER_SIZE], Config.BUFFER_SIZE);
+
+	            datagramSocket.receive(receivedPacket);
+
+	            int length = receivedPacket.getLength();
+	            String message =
+	                    new String(receivedPacket.getData(), 0, length, "utf8");
+
+	            // Port i host który wysłał nam zapytanie
+	            InetAddress address = receivedPacket.getAddress();
+
+	            
+	            System.out.println("Przyjąłem od "+address.toString());
+	            
+	            int port = receivedPacket.getPort();
+	            
+	            
+	            // Reszta kodu dla serwera
+	            Thread.sleep(1000); //To oczekiwanie nie jest potrzebne dla
+	            // obsługi gniazda
+
+	            DatagramPacket response
+	                    = new DatagramPacket(
+	                        byteResponse, byteResponse.length, address, port);
+
+	            datagramSocket.send(response);
+	            
+	            // Na razie spróbuję rozdzielić stringa na część w której znajduje się następny adres
+	            // i część w której jest wiadomość. Powinniśmy to potem ulepszyć?
+	            String[] partsOfString = message.split(";"); // senden message - 1
+	                     
+	            if(partsOfString.length == 1) { // Powrót
+	            	
+	            	if(prevAdress==null) {
+			            // Zapisujemy do pamięci poprzedni adres
+			            prevAdress = address.toString(); // może to jakoś polepszyć. Zrobić pole z adress?
+			            //Na razie InetAdress::toString dodaje / na początku. Usunę to ręcznie
+			            prevAdress = prevAdress.substring(1);
+	            	}
+		            
+	            	System.out.println("Wiadomość dotarła. Odsyłam wiadomość na poprzedni adres: "+prevAdress);
+	            	// Odsyłamy (POPRAWKA)
+	            	if(debug) System.out.println("NEXT ADRESS = "+prevAdress);
+	            	InetAddress serverAddress = InetAddress.getByName(prevAdress); //zamiast "localhost"
+		            System.out.println(serverAddress);
+	
+		            DatagramSocket socket = new DatagramSocket(); //Otwarcie gniazda
+		            byte[] stringContents = returnMessage.getBytes("utf8"); //Pobranie strumienia bajtów z wiadomosci
+	
+		            DatagramPacket sentPacket = new DatagramPacket(stringContents, stringContents.length);
+		            sentPacket.setAddress(serverAddress);
+		            sentPacket.setPort(Config.PORT);
+		            socket.send(sentPacket);
+		            
+		            // Czekamy na odpowiedź
+		            
+		            DatagramPacket recievePacket = new DatagramPacket( new byte[Config.BUFFER_SIZE], Config.BUFFER_SIZE);
+		            socket.setSoTimeout(1010);
+	
+		            try{
+		                socket.receive(recievePacket);
+		                System.out.println("Serwer otrzymał wiadomość");
+		            }catch (SocketTimeoutException ste){
+		                System.out.println("Serwer nie odpowiedział, więc albo dostał wiadomość albo nie...");
+		            }
+		            // Koniec wysyłki
+	            	
+	            }
+	            else if(partsOfString.length > 1) { // Gdy są podane adresy
+	            	
+		            // Zapisujemy do pamięci poprzedni adres
+		            prevAdress = address.toString(); // może to jakoś polepszyć. Zrobić pole z adress?
+		            //Na razie InetAdress::toString dodaje / na początku. Usunę to ręcznie
+		            prevAdress = prevAdress.substring(1);         	
+	            	
+	            	String nextAdress = partsOfString[0];
+	            	String message2Send="";
+	            	for(int i=1;i<partsOfString.length;i++) message2Send += partsOfString[i]+";";
+	            	// Wysyłka
+	            	// UDPClient
+	            	if(debug) System.out.println("Paczka do przesłania\n"+message2Send);
+		            InetAddress serverAddress = InetAddress.getByName(nextAdress); //zamiast "localhost"
+		            System.out.println(serverAddress);
+	
+		            DatagramSocket socket = new DatagramSocket(); //Otwarcie gniazda
+		            byte[] stringContents = message2Send.getBytes("utf8"); //Pobranie strumienia bajtów z wiadomosci
+	
+		            DatagramPacket sentPacket = new DatagramPacket(stringContents, stringContents.length);
+		            sentPacket.setAddress(serverAddress);
+		            sentPacket.setPort(Config.PORT);
+		            socket.send(sentPacket);
+		            
+		            // Czekamy na odpowiedź
+		            
+		            DatagramPacket recievePacket = new DatagramPacket( new byte[Config.BUFFER_SIZE], Config.BUFFER_SIZE);
+		            socket.setSoTimeout(1010);
+	
+		            try{
+		                socket.receive(recievePacket);
+		                System.out.println("Serwer otrzymał wiadomość");
+		            }catch (SocketTimeoutException ste){
+		                System.out.println("Serwer nie odpowiedział, więc albo dostał wiadomość albo nie...");
+		            }
+		            // Koniec wysyłki
+	            }
+	            
+	            else System.out.println("Zła wiadomość");
+	            
+	            
+	            
+	            
+	            
+	            
+       
+
+	            
+//	            sendMessage(sendenMessage,partsOfString);
+	            
+	            // Wysyłam
+	            // UDPClient
+//	            String message2Send =  sendenMessage;
+//	            InetAddress serverAddress = InetAddress.getByName(nextAdress); //zamiast "localhost"
+//	            System.out.println(serverAddress);
+//
+//	            DatagramSocket socket = new DatagramSocket(); //Otwarcie gniazda
+//	            byte[] stringContents = message2Send.getBytes("utf8"); //Pobranie strumienia bajtów z wiadomosci
+//
+//	            DatagramPacket sentPacket = new DatagramPacket(stringContents, stringContents.length);
+//	            sentPacket.setAddress(serverAddress);
+//	            sentPacket.setPort(Config.PORT);
+//	            socket.send(sentPacket);
+//
+//	            DatagramPacket recievePacket = new DatagramPacket( new byte[Config.BUFFER_SIZE], Config.BUFFER_SIZE);
+//	            socket.setSoTimeout(1010);
+//
+//	            try{
+//	                socket.receive(recievePacket);
+//	                System.out.println("Serwer otrzymał wiadomość");
+//	            }catch (SocketTimeoutException ste){
+//	                System.out.println("Serwer nie odpowiedział, więc albo dostał wiadomość albo nie...");
+//	            }
+	            
+	            
+	            
+	        }
+	    }
+	}
